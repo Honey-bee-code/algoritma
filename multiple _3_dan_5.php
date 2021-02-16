3534b9997eea1b5275d8999f3fa88add4fb8fbd2
Honey-bee-code <hanifinida@gmail.com>
2021-02-16 10:42:18 +0800

    add some explanation

--- multiple _3_dan_5.php
+++ multiple _3_dan_5.php
@@ -1,5 +1,16 @@
 <?php
 
+for($i=0; $i<1000; $i = $i +3){
+  echo $i."|";
+}
+echo "<br>";
+for($i=0; $i<1000; $i = $i +5){
+  echo $i."|";
+}
+echo "<br>";
+echo "<br>";
+
+
 $total = 0;
 $max = 1000;
 
@@ -13,6 +24,20 @@ for($i=0; $i<$max; $i++){
 echo "Hasil PHP : ".$total;
 echo "<p>";
 
+// Using the formula 1 + 2 + 3 + ... + n = n*(n+1)/2 instead of a code loop.
+
+// Sum of all numbers <1000 divisible by 3:
+// 3 + 6 + 9 + ... + 999 = 3 * (1 + 2 + 3 + ... + 333) = 3 * 333*334/2 = 166833
+
+// Sum of all numbers <1000 divisible by 5:
+// 5 + 10 + 15 + ... + 995 = 5 * (1 + 2 + 3 + ... + 199) = 5 * 199*200/2 = 99500
+
+// Adding these up, those numbers divisible by 3*5 = 15 are counted double. Therefore we need to subtract these once:
+
+// Sum of all numbers <1000 divisible by 15:
+// 15 + 30 + 45 + ... + 990 = 15 * (1 + 2 + 3 + ... + 66) = 15 * 66*67/2 = 33165
+
+// Thus we get the result 166833 + 99500 - 33165 = 233168.
 
 
 
@@ -28,3 +53,4 @@ for(i = 0; i < 1000; i++){
 }
 document.getElementById('hasil').innerHTML=sum;
 </script>
+
