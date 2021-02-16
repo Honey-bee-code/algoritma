ddb9057a5d25ccac60474a693064d48fdec0c182
Honey-bee-code <hanifinida@gmail.com>
2021-02-16 10:27:51 +0800

    menampilkan dua hasil

--- multiple _3_dan_5.php
+++ multiple _3_dan_5.php
@@ -1,18 +1,24 @@
 <?php
 
-$sum = 0;
-for($i=0; $i<1000; $i++){
+$total = 0;
+$max = 1000;
+
+for($i=0; $i<$max; $i++){
     if($i%3 == 0 || $i%5 == 0) {
-        $sum = $sum + $i;
+        $total = $total + $i;
     }
 }
-echo $sum;
+
+
+echo "Hasil PHP : ".$total;
+echo "<p>";
+
 
 
 
 ?>
 
-<div id="hasil"></div>
+<div>Hasil Javascript : <span id="hasil"></span></div>
 <script>
 var sum = 0;
 for(i = 0; i < 1000; i++){
